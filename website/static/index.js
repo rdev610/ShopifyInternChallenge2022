ce29763eaa5381879d16f2de67ff7b4ee4337269
rdev610 <97998450+rdev610@users.noreply.github.com>
2022-01-19 17:15:06 -0500

    Add files via upload

---
+++ website/static/index.js
@@ -0,0 +1,9 @@
+function deleteProduct(productId) {
+    fetch("/delete-product", {
+      method: "POST",
+      body: JSON.stringify({ productId: productId }),
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+  }
+

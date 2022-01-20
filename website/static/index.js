99ab9ce421e74dbb6ae9b98bf16ac03569c7b389
rdev610 <97998450+rdev610@users.noreply.github.com>
2022-01-19 20:00:29 -0500

    Delete website directory

--- website/static/index.js
+++
@@ -1,9 +0,0 @@
-function deleteProduct(productId) {
-    fetch("/delete-product", {
-      method: "POST",
-      body: JSON.stringify({ productId: productId }),
-    }).then((_res) => {
-      window.location.href = "/";
-    });
-  }
-
